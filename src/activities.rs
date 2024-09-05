967625905f07f6508ecae1860bbdf2d8ed25732c
Daniel Adams <msub2official@gmail.com>
2024-09-05 10:01:47 -1000

    Activities converted

--- src/activities.rs
+++ src/activities.rs
@@ -10,7 +10,7 @@ use url::Url;
 
 use crate::error::Error;
 use crate::{actors::DbRelay, apps::DbApp};
-use crate::{services::RelayAcceptedActivities, AppState};
+use crate::AppState;
 
 #[derive(Deserialize, Serialize, Clone, Debug)]
 #[serde(rename_all = "camelCase")]
@@ -33,22 +33,6 @@ impl Follow {
     }
 }
 
-impl FromRow<'_, sqlx::postgres::PgRow> for DbActivity {
-    fn from_row(row: &PgRow) -> Result<Self, sqlx::Error> {
-        let actor = row.try_get_raw("actor");
-        let actor = actor.unwrap().as_str().unwrap();
-        let object = row.try_get_raw("object");
-        let object = object.unwrap().as_str().unwrap();
-        Ok(Self {
-            actor: ObjectId::parse(actor).unwrap(),
-            object: ObjectId::parse(object).unwrap(),
-            kind: "Follow".to_string(),
-            id: Url::parse(row.try_get("id")?).unwrap(),
-            ap_id: ObjectId::parse(row.try_get("ap_id")?).unwrap(),
-        })
-    }
-}
-
 #[async_trait::async_trait]
 impl ActivityHandler for Follow {
     type DataType = AppState;
@@ -68,26 +52,28 @@ impl ActivityHandler for Follow {
 
     async fn receive(self, data: &Data<Self::DataType>) -> Result<(), Self::Error> {
         let actor = self.actor.dereference(data).await?;
-        unsafe {
-            let new_relay = DbRelay::new(
-                actor.name.clone(),
-                actor.ap_id.clone(),
-                actor.inbox.clone(),
-                actor.outbox.clone(),
-                actor.public_key_pem().to_string(),
-                None,
-                false,
-            );
-            RELAYS.push(new_relay)
-        }
-        unsafe {
-            ACTIVITIES.push(RelayAcceptedActivities::Follow(self));
-        }
+        sqlx::query("INSERT INTO relays (name, ap_id, inbox, outbox, public_key, private_key, local) VALUES ($1, $2, $3, $4, $5, $6, $7)")
+            .bind(&actor.name)
+            .bind(&actor.ap_id.inner().as_str())
+            .bind(&actor.inbox.as_str())
+            .bind(&actor.outbox.as_str())
+            .bind(&actor.public_key_pem())
+            .bind(None::<String>)
+            .bind(false)
+            .execute(&data.db)
+            .await?;
+        sqlx::query("INSERT INTO activities (actor, object, kind, activity_id) VALUES ($1, $2, $3, $4)")
+            .bind(&self.actor.inner().as_str())
+            .bind(&self.object.inner().as_str())
+            .bind("Follow")
+            .bind(&self.id.as_str())
+            .execute(&data.db)
+            .await?;
         Ok(())
     }
 }
 
-#[derive(Deserialize, Serialize, Clone, Debug, FromRow)]
+#[derive(Deserialize, Serialize, Clone, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct Create {
     pub actor: ObjectId<DbRelay>,
@@ -116,10 +102,21 @@ impl ActivityHandler for Create {
 
     async fn receive(self, data: &Data<Self::DataType>) -> Result<(), Self::Error> {
         let experience = self.object.dereference(data).await?;
-        unsafe {
-            APPS_LIST.push(experience);
-            ACTIVITIES.push(RelayAcceptedActivities::Create(self));
-        }
+        sqlx::query("INSERT INTO apps (activitypub_id, url, name, description, is_active) VALUES ($1, $2, $3, $4, $5)")
+            .bind(&experience.ap_id.inner().as_str())
+            .bind(&experience.url)
+            .bind(&experience.name)
+            .bind(&experience.description)
+            .bind(&experience.active)
+            .execute(&data.db)
+            .await?;
+        sqlx::query("INSERT INTO activities (actor, object, kind, activity_id) VALUES ($1, $2, $3, $4)")
+            .bind(&self.actor.inner().as_str())
+            .bind(&self.object.inner().as_str())
+            .bind("Create")
+            .bind(&self.id.as_str())
+            .execute(&data.db)
+            .await?;
         Ok(())
     }
 }
@@ -130,4 +127,20 @@ pub struct DbActivity {
     pub object: ObjectId<DbApp>,
     pub id: Url,
     pub kind: String,
+}
+
+impl FromRow<'_, sqlx::postgres::PgRow> for DbActivity {
+    fn from_row(row: &PgRow) -> Result<Self, sqlx::Error> {
+        let actor = row.try_get_raw("actor");
+        let actor = actor.unwrap().as_str().unwrap();
+        let object = row.try_get_raw("object");
+        let object = object.unwrap().as_str().unwrap();
+        Ok(Self {
+            actor: ObjectId::parse(actor).unwrap(),
+            object: ObjectId::parse(object).unwrap(),
+            kind: "Follow".to_string(),
+            id: Url::parse(row.try_get("id")?).unwrap(),
+            ap_id: ObjectId::parse(row.try_get("ap_id")?).unwrap(),
+        })
+    }
 }
