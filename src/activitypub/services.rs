0cb488dc9626c4ba8e172219b478c6e0240de269
Daniel Adams <msub2official@gmail.com>
2024-11-12 11:37:39 -1000

    Filter out hidden apps from the front carousel

--- src/activitypub/services.rs
+++ src/activitypub/services.rs
@@ -80,6 +80,7 @@ async fn index(data: Data<AppState>) -> impl Responder {
             if !data.debug {
                 apps.retain(|app| !app.url.contains("localhost"));
             }
+            apps.retain(|app| app.visible);
             let total_apps_count = apps.len();
             let mut unique_urls = HashSet::new();
             apps.retain(|app| {
